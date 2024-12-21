4e9bc628111dcb2217d709c5975db1122a716731
Koray Vatansever <koray-vatansever@hotmail.com>
2024-12-21 20:33:56 +0300

    Add project files.

---
+++ Test_List_Ornek/Arabalar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_List_Ornek
+{
+    internal class Arabalar
+    {
+        string marka;
+        string model;
+        int yil;
+        string renk;
+
+        public string MARKASI
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+        public string MODELI
+        {
+            get { return model; }
+            set {model = value; }
+        }
+        public int YILI
+        {
+            get { return yil; }
+            set { yil = value; }
+        }
+        public string RENK
+        {
+            get { return renk; }
+            set { renk = value; }
+        }
+    }
+}
---
+++ Test_List_Ornek/Kisiler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_List_Ornek
+{
+    internal class Kisiler
+    {
+        string ad;
+        string soyad;
+        string meslek;
+
+        public string ADI
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+
+        public string SOYADI
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+
+        public string MESLEKI
+        {
+            get { return meslek; }
+            set { meslek = value; }
+        }
+    }
+}
